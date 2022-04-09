adbc124991975d72a7654481bcbc01fe7f41a9d9
PeppinoFreeman <compte.suprise02@gmail.com>
2022-04-09 18:41:49 +0200

    navigation to page About

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -3,17 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about.component";
 
 const routes: Routes = [
-	{
-		path: "",
-		component: AboutComponent
-	}
+  {
+    path: "",
+    component: AboutComponent,
+  },
 ];
 
 @NgModule({
-	imports: [
-		RouterModule.forChild(routes)
-	],
-	exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AboutRoutingModule {
-}
+export class AboutRoutingModule {}
