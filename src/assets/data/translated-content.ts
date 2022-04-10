73a2416aebabf7d85fa22485405c3dea79f66426
PeppinoFreeman <compte.suprise02@gmail.com>
2022-04-10 15:19:03 +0200

    fixed person generation with criterias

--- src/assets/data/translated-content.ts
+++ src/assets/data/translated-content.ts
@@ -3,10 +3,18 @@ import { TranslationLang } from "src/app/enums/translation-languages";
 const contentFr = {
   MALE: "homme",
   FEMALE: "femme",
+  GENDER: "genre",
+  EMAIL: "email",
+  FIRSTNAME: "prénom",
+  LASTNAME: "nom",
 };
 const contentEn = {
   MALE: "male",
   FEMALE: "female",
+  GENDER: "gender",
+  EMAIL: "email",
+  FIRSTNAME: "firstName",
+  LASTNAME: "lastName",
 };
 
 export const translatedContent = {
